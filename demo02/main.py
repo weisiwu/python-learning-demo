8ad823f0ea07491ab9c98d5cdd893849bf9be60a
weisiwu <siwu.wsw@autonavi.com>
2022-07-15 16:12:00 +0800

    chore: demo2 complete[Frist Branch]: master

--- demo02/main.py
+++ demo02/main.py
@@ -18,15 +18,8 @@ for i in range(0, 3):
   tmp = list('abcedfghijklmnopqrstuvwxyzABCEDFGHIJKLMNOPQRSTUVWXYZ0123456789')
   random.shuffle(tmp)
   trans_arr.append(''.join(tmp))
-set(trans_arr)
-# 去重,并重新添加
-while len(trans_arr) < 3:
-  tmp = list('abcedfghijklmnopqrstuvwxyzABCEDFGHIJKLMNOPQRSTUVWXYZ0123456789')
-  random.shuffle(tmp)
-  trans_arr.append(''.join(tmp))
-  set(trans_arr)
 # 生成基数组: 001001001...
-base_arr = [ str(x).zfill(3)*3 for x in range(0, 200)]
+base_arr = [ str(x).zfill(3)*3 for x in range(0, 200) ]
 # 打开文件
 output = open('activeCode.txt', 'w')
 # 转化基数组为结果数组
@@ -42,4 +35,4 @@ for item in base_arr:
   # 将生成的激活码写入文件
   output.write(pa + pb + pc + '\n')
 
-print('file completed!')
+print('200个随机码生成完毕!')
