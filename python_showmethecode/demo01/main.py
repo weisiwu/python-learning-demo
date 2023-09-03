7af9b329830b47eb3b43f026dc229197259e4716
weisiwu <2436887475@qq.com>
2023-09-03 20:28:09 +0800

    feat: demo001 验证码

--- python_showmethecode/demo01/main.py
+++ python_showmethecode/demo01/main.py
@@ -1,38 +1,164 @@
-##-*-coding: utf-8-*-
+import os
 import random
+from datetime import datetime
+import numpy as np
+from PIL import Image, ImageDraw, ImageFont, ImageOps
 
-# 思路描述:
-# 生成9位的兑换码,由数字大小写构成
-# @num {int} 指定生成的兑换码数目
-# 生成算法描述:
-# 1. 先准备一个包含[a-zA-Z0-9]的数组(不重复,62位)
-# 2. 生成一个长200的结果数组,值的特征为: 001001001,即
-# 分为3组,每组都为同样其序号值.
-# 3. 对结果数组中的数值进行转化,将其每一位作为序号,换取
-# 第一步准备的转换数组中对应的值.每转换3次,打乱一次转换
-# 数组的顺序
+"""
+验证码: VerifyCode
+1、有效期限：确定验证码的有效期，防止过期验证码被使用。
+2、唯一性：确保生成的验证码在同一时间内是唯一的，避免重复使用。
+3、由验证码文字生成图片
+4、提高验证码复杂度: 对图片内容进行扭曲变形、噪点干扰、调整字符间距、样式、颜色随机变化等、
+"""
 
-trans_arr = result = []
-# 生成3个不同的转换数组
-for i in range(0, 3):
-  tmp = list('abcedfghijklmnopqrstuvwxyzABCEDFGHIJKLMNOPQRSTUVWXYZ0123456789')
-  random.shuffle(tmp)
-  trans_arr.append(''.join(tmp))
-# 生成基数组: 001001001...
-base_arr = [ str(x).zfill(3)*3 for x in range(0, 200) ]
-# 打开文件
-output = open('activeCode.txt', 'w')
-# 转化基数组为结果数组
-for item in base_arr:
-  pa = pb = pc = ''
-  # 转化
-  for i in item[0:3]:
-    pa += trans_arr[0][int(i)]
-  for j in item[3:6]:
-    pb += trans_arr[1][int(j)]
-  for k in item[6:9]:
-    pc += trans_arr[2][int(k)]
-  # 将生成的激活码写入文件
-  output.write(pa + pb + pc + '\n')
 
-print('200个随机码生成完毕!')
+# 如何写到文件中
+# 如何绘制图片
+class VerifyCode:
+    code = ""
+    default_config = {
+        # 验证码长度
+        "verify_code_len": 6,
+        # 验证码字符集
+        "verify_code_set": "1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz",
+        # 7200秒
+        "verify_code_expire_time": 7200,
+        "verify_code_fontsize": 50,
+        "verify_code_height": 80,
+        "verify_code_width": 250,
+        "verify_code_save_path": None,
+    }
+    meme_code = {}
+
+    def __init__(self, uuid, config=default_config):
+        self.config = config if config is not None else self.default_config
+        self.uuid = uuid
+        self.code_set_len = len(self.config["verify_code_set"])
+
+    def get_current_path(self):
+        return os.path.dirname(__file__)
+
+    def get_random_color(self):
+        color = []
+
+        for index in range(0, 3):
+            color.append(random.randint(0, 255))
+
+        return tuple(color)
+
+    def get_random_size(self):
+        font_size = self.config["verify_code_fontsize"]
+
+        return random.randint(int(font_size * 0.85), font_size)
+
+    def get_memo_verify_code(self):
+        try:
+            code_memo = self.meme_code[self.uuid]["code"]
+            start_time = self.meme_code[self.uuid]["time"]  # 生成时间
+
+            if start_time + self.config["verify_code_expire_time"] >= datetime.now():
+                return code_memo
+            else:
+                return ""
+        except KeyError:
+            return ""
+
+    def verify_code_draw(self):
+        width = self.config["verify_code_width"]
+        height = self.config["verify_code_height"]
+        size = self.config["verify_code_fontsize"]
+
+        image = Image.new("RGB", (width, height), "white")
+
+        image_draw = ImageDraw.Draw(image)
+
+        for index, text in enumerate(self.code):
+            # 随机大小和颜色
+            font_size = self.get_random_size()
+            font = ImageFont.truetype(
+                f"{self.get_current_path()}/ttf/From Cartoon Blocks.ttf", size=font_size
+            )
+            fill_color = self.get_random_color()
+            # 表示如果是第一个字，左侧间距是17，后续的依次加一个字宽
+            position = (17 if index == 0 else 36 * index + 17, (height - size) / 2)
+            image_draw.text(position, text, fill=fill_color, font=font)
+
+            # 扭曲文本
+            twisted_text = text
+            twisted_text_image = Image.new("L", (width, height))
+            draw_twisted = ImageDraw.Draw(twisted_text_image)
+            draw_twisted.text(position, twisted_text, font=font, fill=255)
+
+            # 使用滤镜来扭曲文本
+            # TODO:(wsw) 没有看到扭曲效果
+            twisted_text_image = twisted_text_image.transform(
+                (width, height),
+                Image.PERSPECTIVE,
+                (1.0, 0.2, -50.0, 0.0, 1.5, -100.0, 0.0, 0.001, 1.0),
+                Image.BILINEAR,
+            )
+            image.paste(
+                ImageOps.colorize(twisted_text_image, (0, 0, 0), (255, 0, 0)), (50, 100)
+            )
+
+        save_path = f"{self.get_current_path()}/output"
+
+        # 如果调用的时候，传入了保存路径，则不使用默认路径
+        if self.config["verify_code_save_path"]:
+            save_path = self.config["verify_code_save_path"]
+        elif not os.path.isdir(save_path):
+            # makedirs 可以递归创建目录
+            os.makedirs(save_path)
+
+        try:
+            image.save(f"{save_path}/{self.code}.png")
+        except Exception as e:
+            # 防止传入的目录不可用
+            image.save(f"{self.get_current_path()}/output.png")
+            print(f"\033[91m传入的路径（verify_code_save_path）不可用，错误信息如下：\033[0m")
+            print(f"\033[91m{e}\033[0m")
+
+        image.show()
+
+    def verify_code_generate(self):
+        code_arr = [
+            self.config["verify_code_set"][random.randint(0, self.code_set_len - 1)]
+            for index in range(0, self.config["verify_code_len"])
+        ]
+        self.code = "".join(code_arr)
+
+        # 绘制验证码图片&&提高图片的复杂度
+        self.verify_code_draw()
+
+        self.meme_code[self.uuid] = {
+            "time": datetime.now().timestamp(),
+            "code": self.code,
+        }
+
+    # 针对特定id深处验证码，如果没有传入，则从本地缓存读取未过期的验证码返回
+    def verify_code(self):
+        if self.uuid:
+            verify_code_memo = self.get_memo_verify_code()
+            if not verify_code_memo:
+                self.verify_code_generate()
+        else:
+            self.verify_code_generate()
+
+        return self.code
+
+    def get_verify_code_info(self):
+        return self.meme_code[self.uuid]
+
+
+"""
+激活码&&验证码
+激活码: ActivateCode
+"""
+
+if __name__ == "__main__":
+    print(f"TEST CASE 1 START =======================\n")
+    uuid = "test_1"
+    code = VerifyCode(uuid).verify_code()
+    print(f"为{uuid}生成的验证码为: {code}\n")
+    print(f"TEST CASE 1 END =======================\n")
