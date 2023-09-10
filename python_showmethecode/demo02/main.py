67895c3956e20b30aca11b2de12226a87aaf1dff
weisiwu <2436887475@qq.com>
2023-09-10 22:24:21 +0800

    feat: 添加修改文件

--- python_showmethecode/demo02/main.py
+++ python_showmethecode/demo02/main.py
@@ -1,42 +1,69 @@
 import os
-import time
-import random
-import MySQLdb
-from ..demo01.main import ActivateCode
-
-print(f"ActivateCode {ActivateCode}")
+from MySQLdb import _mysql
 
 
-def connect_db():
-    pass
+class CodeTable:
+    def __init__(self):
+        self.db = _mysql.connect(
+            host="localhost",
+            user="root",
+        )
+        self.cursor = self.db.cursor()
 
+    def create_database(self, database):
+        database_query_result = self.cursor.execute("show databases;")
+        print(f"database_query_result {database_query_result}")
 
-def create_table():
-    pass
+        # 创建数据库
+        create_database_result = self.cursor.execute(f"CREATE DATABASE {database};")
+        print(f"create_database_result {create_database_result}")
 
+        # 选择数据库
+        self.cursor.execute("use demo02;")
+        self.db.commit()
 
-def append_data():
-    pass
+        return self
+
+    def create_table(self, table):
+        table_query_result = self.cursor.execute("show tables;")
+
+        create_table_result = self.cursor.execute(
+            f"""
+            CREATE TABLE IF NOT EXISTS {table} (
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                code VARCHAR(255) NOT NULL
+            )
+            """
+        )
+
+        self.db.commit()
+
+        return self
+
+    def insert_data(self, code):
+        database_query_result = self.cursor.execute(
+            """
+            INSERT INTO users (code)
+            VALUES (%s)
+            """,
+            (code),
+        )
+        self.db.commit()
+
+        return self
 
 
 if __name__ == "__main__":
     batch_number = 200
-    # 生成激活码，并保存实例
-    activate_ins = ActivateCode(
-        config={
-            "batch_number": batch_number,
-            # 7200秒
-            "valid_date": time.time() + 7e6 + 2e5,
-        }
-    )
-    activate_code = activate_ins.encrypt_batch()
-    random_index = random.randint(0, batch_number)
 
-    with open(
-        os.path.join("../demo01", os.path.dirname(__file__), "./activeCodeList.txt"),
-        "r",
-    ) as file:
-        for _ in range(0, batch_number):
-            print(f"code是什么 {code}")
-            code = file.readline(batch_number)
-            append_data(code)
+    code_db = CodeTable()
+    code_db.create_database("demo02").create_table("activate_code")
+
+    # with open(
+    #     os.path.join(os.path.dirname(__file__), "../demo01/activeCodeList.txt"),
+    #     "r",
+    # ) as file:
+    #     for _ in range(0, batch_number):
+    #         # print(f"code是什么 {code}")
+    #         code = file.readline(batch_number)
+    #         append_data(code)
--- python_showmethecode/demo02/readme.md
+++ python_showmethecode/demo02/readme.md
@@ -4,39 +4,19 @@
 
 将 0001 题生成的 200 个激活码（或者优惠券）保存到 **MySQL** 关系型数据库中。
 
-需要引入之前创建的函数。因为不在统一目录下，可用以下方式（任选其一）：
-
-1、将需要引用的目录添加到`python`路径下
-
-```python
-import sys
-sys.path.append('/path/to/directory')
-
-from main import ActivateCode
-```
-
-2、相对导入
-使用点表示相对路径
-
-```python
-# 使用点表示路径
-from ..demo01.main import ActivateCode
-```
-
-3、使用绝对路径导入
-
-```python
-# 使用绝对路径
-from /path/to/directory/demo01.main import ActivateCode
-```
-
-同之前相同，创建虚拟环境 `db`
+本次依旧通过 `venv` 创建隔离环境`db`。创建成功激活虚拟环境
 
 这里控制 `mysql` 数据库用的是 [`mysqlclient`](https://mysqlclient.readthedocs.io/)
 
 ```python
 # 包虽然叫做 mysqlclient，引入的是 MySQLdb
 import MySQLdb
+```
+
+运行程序
+
+```python
+python main.py
 ```
 
 ## 运行结果
